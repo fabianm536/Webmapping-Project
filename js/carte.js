9469977c6e09abf8cb891d5892f8a052fc3a56cd
Fabian <fabianm536@gmail.com>
2018-12-18 13:58:19 +0100

    Bibliographie

--- js/carte.js
+++ js/carte.js
@@ -1,6 +1,5 @@
 var map,
-	fields = ["scanombre"], 
-	autocomplete = [];
+	fields = ["scanombre"];
 
 
 function initialize(){
@@ -8,8 +7,8 @@ function initialize(){
 	var map = L.map('map',{
 	minZoom: 2,
 	maxZoom: 22,
-	zoomControl: false              
-	}).setView([4.55, -74.1], 12);   	
+	zoomControl: true              
+	}).setView([4.65, -74.1], 12);   	
 
 	var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'); 
 
@@ -27,81 +26,24 @@ function initialize(){
 	opacity : 0.50,
 	format: 'image/png'
 	}).addTo(map);
-
-	var zoom_bar = new L.Control.ZoomBar({position: 'topright'}).addTo(map);
-
-	//next: add features to map
-	getData();
-};
-
-
-
-// Couche des secteurs depuis un script PHP
-
-
-
-function getData(){
-	$.ajax("php/secteurs.php", {
-		data: {
-			table: "fracsandsites",
-			fields: fields
-		},
-		success: function(data){
-			mapData(data);
-		}
+	
+	
+	var secteurs = $.ajax({
+	  url:"http://localhost/Webmapping-Project/php/getData.php?table=secteur_cadastral&fields[]=scanombre",
+	  dataType: "json",
+	  success: console.log("County data successfully loaded."),
+	  error: function (xhr) {
+	alert(xhr.statusText)
+	}
 	})
-};
 
-function mapData(data){
-	//remove existing map layers
-	map.eachLayer(function(layer){
-		//if not the tile layer
-		if (typeof layer._url === "undefined"){
-			map.removeLayer(layer);
-		}
+	// Specify that this code should run once the  data request is complete
+	$.when(secteurs).done(function() {
+		var secteursData = L.geoJSON(secteurs.responseJSON).addTo(map);
 	});
 
-	//create geojson container object
-	var geojson = {
-		"type": "FeatureCollection",
-		"features": []
-	};
+}
 
-	//split data into features
-	var dataArray = data.split(", ;");
-	dataArray.pop();
-    
-    //console.log(dataArray);
-	
-	//build geojson features
-	dataArray.forEach(function(d){
-		d = d.split(", "); //split the data up into individual attribute values and the geometry
 
-		//feature object container
-		var feature = {
-			"type": "Feature",
-			"properties": {}, //properties object container
-			"geometry": JSON.parse(d[fields.length]) //parse geometry
-		};
 
-		for (var i=0; i<fields.length; i++){
-			feature.properties[fields[i]] = d[i];
-		};
 
-		//add feature names to autocomplete list
-		if ($.inArray(feature.properties.featname, autocomplete) == -1){
-			autocomplete.push(feature.properties.featname);
-		};
-
-		geojson.features.push(feature);
-	});
-	
-    //console.log(geojson);
-    
-    //activate autocomplete on featname input
-    $("input[name=featname]").autocomplete({
-        source: autocomplete
-    });
-
-	var mapDataLayer = L.geoJson(geojson).addTo(map);
-};
