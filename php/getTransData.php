9bf7803fcca9357f79a4744dab17247bf359d8aa
Fabian <fabianm536@gmail.com>
2019-01-11 12:53:20 +0100

    transport data

---
+++ php/getTransData.php
@@ -0,0 +1,38 @@
+<?php 
+
+require 'conndb.php';
+
+$Xadresse = $_GET['xadresse'];
+$Yadresse = $_GET['yadresse'];
+
+$sql = "select ST_AsGeoJSON(ST_Transform(geom,4326)) as geojson, ntrnombre, (MIN(round(cast(distance as numeric),2))) dist_min
+from (with index_query as (
+  select 
+    ntrnombre, geom,
+	(st_distance(geom, st_setsrid(st_makepoint($Xadresse,$Yadresse),4686)))*40030173.5920411/360 as distance
+  from node_transport 
+  order by geom <#> st_setsrid(st_makepoint($Xadresse,$Yadresse),4686) limit 100
+)
+select ntrnombre, distance, geom from index_query ) as res
+group by ntrnombre, geojson order by dist_min limit 10";
+
+$result = pg_query($conexion,$sql);
+
+if (!$result) {
+  echo "An error occurred.\n";	
+  exit;
+}
+
+$feature = array(); 
+
+while ($row = pg_fetch_assoc($result)) { 
+	$res['ntrnombre'] = $row['ntrnombre'];
+	$res['dist_min'] = $row['dist_min'];
+	$geom = $row['geojson']; 
+
+	$feature[] = '{"type": "Feature", "geometry": ' . $geom . ', "properties": ' . json_encode($res) . '}';;
+} 
+ 
+echo '{"type": "FeatureCollection", "features": [' . implode(', ',$feature) . ']}'; 
+
+?>
